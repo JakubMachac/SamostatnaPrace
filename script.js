726a8c72a4bfc72dad9ff178a66fbcaa6b7f09b8
Jakub Machač <machynkar@gmail.com>
2020-11-03 08:14:53 +0000

    .tabulka, class=tabulka, prvni prikaz pred podminku

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ function kvadra() {
   let c = document.getElementById("c").valueAsNumber;
 
   let d = (b*b)-(4*a*c);
-
+document.getElementById("Dis").innerHTML=d;
   if (d < 0){
-    document.getElementById("Dis").innerHTML=d;
+    
     document.getElementById("x1").innerHTML="rovnice nemá řešení";
     document.getElementById("x2").innerHTML="rovnice nemá řešení";
   } else if (d == 0){
-    document.getElementById("Dis").innerHTML=d;
+    
     x1 = -b/(2*a);
     document.getElementById("x1").innerHTML=x1;
     document.getElementById("x2").innerHTML="shodné s x1";
   } else {
-    document.getElementById("Dis").innerHTML=d;
+   
     x1 = (-b + Math.sqrt(d))/(2*a);
     x2 = (-b - Math.sqrt(d))/(2*a);
     document.getElementById("x1").innerHTML=x1;
@@ -40,20 +40,20 @@ function fibpos() {
 }
 function prvcisl() {
   let pocet = document.getElementById("cislo").valueAsNumber;
-   let vypis = " ";
+   let napsat = " ";
   let count = 0;
   let num = 1;
   
   while (count < pocet) {
     if (P(num)) {
-      vypis = vypis + num + " ";
+      napsat = napsat + num + " ";
       num = num + 1;
       count = count + 1;
     } else {
       num = num + 1;
     }
   }
-  document.getElementById("vypis").innerHTML = vypis;
+  document.getElementById("napsat").innerHTML = napsat;
 }
 
 function P(c) {
